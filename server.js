3b6bd4a4917fdd70c8d0a3a73035561217fce41f
Mateo Vargas Giraldo <mateovg.1943@gmail.com>
2024-04-12 12:13:20 -0500

    port change from 3000 to 4900

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4900;
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
