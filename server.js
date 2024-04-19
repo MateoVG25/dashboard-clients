0477cce7a9b836776503e1f368109c4cc4ab743c
Mateo Vargas Giraldo <mateovg.1943@gmail.com>
2024-04-19 12:22:13 -0500

    error merging

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 4901;
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
-const app = next({ dev });
-const handle = app.getRequestHandler();
+
 
 app.prepare().then(() => {
   createServer((req, res) => {
